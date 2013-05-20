eab48a0a1a1c6d7234eb61b80f8711e2391b4b5e
Joshua Westgard <westgardja@gmail.com>
2013-05-20 07:47:55 -0400

    separate a few functions, continue developing METS process

--- test.py
+++ test.py
@@ -4,36 +4,44 @@ def greeting():             # A dummy function to initiate interaction with the 
     print("\nHello " + name + ", welcome to the XML generator!")
     currentTime = datetime.datetime.utcnow()
     print('It is now ' + str(currentTime))       # prints the current time
-    print("\nThis program is designed to take data from a CSV file and populate" +
+    print("\nThis program is designed to take data from a CSV file and populate " +
           "an XML template with that data in order to generate FOXML files for the University" +
           "of Maryland's digital collections repository.")
 
-def getPIDs(numPIDs):       # This function retrieves a specified number of PIDs 
+def getPids(numPids):       # This function retrieves a specified number of PIDs 
     import requests         # from Fedora server.
-    import re
     url = 'http://fedoradev.lib.umd.edu/fedora/management/getNextPID?numPids='
-    url += '{0}&namespace=umd&xml=true'.format(numPIDs)
+    url += '{0}&namespace=umd&xml=true'.format(numPids)
     username = input('\nEnter the server username: ')       # prompts user for auth info
     password = input('Enter the server password: ')
     f = requests.get(url, auth=(username, password)).text   # submits request to fedora server
     print("\nRetrieving PIDs from the server...")
-    pidList = []                                               # create list to hold PIDs
     print('\nServer answered with the following XML file:\n')  # print server's response
     print(f)
-    for line in f.splitlines():                             # for each line in the response
+    fName = input('Enter a name to save the server\'s PID file: ')
+    writeFile(fName, '', f)
+    return f
+
+def parsePids(pidFile):
+    import re
+    pidList = []                                            # create list to hold PIDs
+    for line in pidFile.splitlines():                       # for each line in the response
         pid = re.search('<pid>(.*?)</pid>', line)           # search for PID and if found
         if pid:
             pidList.append(pid.group(1))                    # append each PID to list
-    print('Successfully reserved the following ', end='')
+    print('Successfully loaded the following ', end='')
     print('{0} PIDs: '.format(len(pidList)) + str(pidList)) # print result 
     return pidList                                          # return the PID list
 
-# Prompt the user to enter the name of the UMAM or UMDM template to be imported for populating
-# with the data from the CSV file.
-def loadTemplate(fileType):
-    templatename = input("\nEnter the name of the %s template: " % (fileType))
-    template = open(templatename, "r").read()
-    return(template)
+# Prompt the user to enter the name of the UMAM or UMDM template or PID file and
+# read that file, returning the contents.
+def loadFile(fileType):
+    sourceFile = input("\nEnter the name of the %s file: " % (fileType))
+    if fileType == 'data':
+        f = open(sourceFile, 'r').readlines()
+    else:
+        f = open(sourceFile, 'r').read()
+    return(f)
 
 # Creates a file containing the contents of the "content" string, named [fileName]_[fileType].xml,
 # files are saved in a sub-directory called "output".
@@ -103,45 +111,62 @@ def createMETS():
  
 def main():
     import csv
-    dataFile = input("\nEnter the name of the data file: ")
-    dataLength = len(dataFile)      # Gets number of lines of data.
-    print("The datafile you specified has {0} rows".format(dataLength))
-    PIDlist = getPIDs(dataLength)   # Gets as many PIDs as lines of data.
-    umam = loadTemplate('UMAM')     # Loads UMAM template.
+    greeting()
+    dataFile = loadFile('data')
+    dataLength = len(dataFile)
+    print('The datafile you specified has {0} rows.'.format(dataLength))
+    print('Load PIDs from a file or request them from the server?')
+    pidMethod = input('Enter F (file) or S (server): ')
+    pidList = []
+    if pidMethod == 'F':
+        pidFileName = input('Enter the name of the PID file: ')
+        pidFile = open(pidFileName, 'r').read()
+    elif pidMethod == 'S':
+        pidFile = getPids(dataLength)   # Requests as many PIDs as lines of data.
+    else:
+        print('Invalid response.')
+        quit()
+    pidList = parsePids(pidFile)    # Parses PIDs from the PID file (either local or from the server)
+    if len(pidList) < dataLength:
+        print('Not enough PIDs for that dataset. Exiting program.')
+        quit()
+    umam = loadFile('UMAM')         # Loads UMAM template.
     print("\n UMAM:\n" + umam)      # Prints UMAM.
     print('*' * 30)                 # Prints a divider.
-    umdm = loadTemplate('UMDM')     # Loads UMDM template.
+    umdm = loadFile('UMDM')         # Loads UMDM template.
     print("\n UMDM:\n" + umdm)      # Prints UMDM.
     print('*' * 30)                 # Prints a divider.
-    with open(dataFile, mode='r', encoding='utf-8') as inputFile:
-        myData = csv.DictReader(inputFile)
-        i = 0
-        mets = ""
-        objectParts = 0                 # a counter for number of UMAM parts
-        for x in myData:
-            x['PID'] = PIDlist[i]       # Attaches the PID to the dataset.
-            i += i
-            print('\n' + ('*' * 30))
-            print("\nDATASET " + str(i) + " : " + str(x))   # Prints the dataset (key/value pairs).
-            if x['XML_Type'] == 'UMDM':
-                if mets != "":
-                    print('Creating UMDM for object with {0} parts!\n'.format(objectParts))
-                    objectParts = 0
-                    myFile = createUMDM(tempData, umdm)
-                    myFile = myFile.replace('!!!INSERT_METS_HERE!!!', mets)
-                    writeFile(tempData['Item Control Number'].strip(), 'umdm', myFile)
-                tempData = x
-                mets = createMETS()
-            elif x['XML_Type'] == 'UMAM':             # checks whether it's a UMAM row
-                objectParts += objectParts
-                outputFile = createUMAM(x, umam)      # if yes, calls function to populate UMAM template
-                writeFile(x['File Name'].strip(), 'umam', outputFile)   # writes output to file
+    myData = csv.DictReader(dataFile)
+    i = 0
+    mets = ""
+    objectParts = 0                 # a counter for number of UMAM parts
+    for x in myData:
+        x['PID'] = pidList[i]       # Attaches the PID to the dataset.
+        i += 1
+        if x['XML_Type'] == 'UMDM':
+            if mets != "":
+                print('Creating UMDM for object with {0} parts!\n'.format(objectParts))
+                objectParts = 0    
+                myFile = createUMDM(tempData, umdm)
+                myFile = myFile.replace('!!!INSERT_METS_HERE!!!', mets)
+                print('Writing UMDM...')
+                writeFile(tempData['Item Control Number'].strip(), 'umdm', myFile)
+                filesWritten += 1
+            tempData = x
+            mets = createMETS()
+        elif x['XML_Type'] == 'UMAM':             # checks whether it's a UMAM row
+            objectParts += 1
+            outputFile = createUMAM(x, umam)      # if yes, calls function to populate UMAM template
+            print('Writing UMAM...')
+            writeFile(x['File Name'].strip(), 'umam', outputFile)   # writes output to file
+            filesWritten += 1
     print('\n' + ('*' * 30))        # Print a divider and summarize output.
-    print('\n{0} files written. Thanks for '.format(numFiles), end='')
+    print('\n{0} files written. Thanks for '.format(filesWritten), end='')
     print('using the XML generator!\n\n')
     
 def test():
-    testme = createMETS()
-    print(testme)
+    metadata = readData()
+    for x in metadata:
+        print(x)
     
 main()
