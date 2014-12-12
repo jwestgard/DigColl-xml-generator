54ede5d092a52e11d2d39a708e4ffb8d59785cab
Joshua Westgard <jwestgard@mac.com>
2014-12-12 11:03:52 -0500

    remove admin dir
    This directory contains unneeded test files only.

--- admin/passwordtest.py
+++
@@ -1,9 +0,0 @@
-def fetchWebpage(url):
-    import requests
-    page = requests.get(url)
-    return page
-
-p = fetchWebpage('http://www.cnn.com')
-print(p.content)
-import sys
-print(sys.version)
