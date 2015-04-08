3e0084d2253b45d23e0175cb462f05fc1b6cea95
Joshua Westgard <jwestgard@mac.com>
2015-04-08 13:15:54 -0400

    streamline ssjoin.sh

--- ssjoin.sh
+++ ssjoin.sh
@@ -3,17 +3,7 @@
 METADATACSV=$1
 LINKCSV=$2
 
-# Set up header row for a temp file to hold the Sharestream data
-echo "asset,SharestreamURLs" > sstemp.csv
-
-# Copy needed columns from Sharestream download to the temp file
-sed 1d $LINKCSV | csvcut -c1,4 >> sstemp.csv
-
-# Remove SharestreamURLs column from metadata CSV
-csvcut -C "SharestreamURLs" < $METADATACSV > metatemp.csv
-
 # Join the two files
-csvjoin -c "FileName,asset" --left metatemp.csv sstemp.csv > joined.csv
-
-# Clean up
-rm sstemp.csv metatemp.csv
+csvjoin -c "FileName,asset" --left \
+  <(csvcut -C "SharestreamURLs" < $METADATACSV) \
+  <(echo "asset,SharestreamURLs"; sed 1d $LINKCSV | csvcut -c1,3)
