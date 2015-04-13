699ce95f49b68196bb68e1514b18ff9370b35429
Joshua Westgard <jwestgard@mac.com>
2015-04-13 16:53:02 -0400

    get sharestream columns by name rather than position

--- ssjoin.sh
+++ ssjoin.sh
@@ -6,4 +6,5 @@ LINKCSV=$2
 # Join the two files
 csvjoin -c "FileName,asset" --left \
   <(csvcut -C "SharestreamURLs" < $METADATACSV) \
-  <(echo "asset,SharestreamURLs"; sed 1d $LINKCSV | csvcut -c1,3)
+  <(echo "asset,SharestreamURLs"; sed 1d $LINKCSV | \
+  csvcut -c "Asset Title, Streamable Deeplink URL")
